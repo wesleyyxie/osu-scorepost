97cb74a5e8b2c0428f7b73ac5666818e3b216cc4
wesleyyxie <wesleyxie05@gmail.com>
2024-08-22 20:23:13 -0400

    added gunicorn option

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app.main import app
-import sys
+
+app = app
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "host":
-        host = "0.0.0.0"
-    else:
-        host = ""
-    app.run(debug=True, host=host)
+    app.run(debug=True)
